f1ac7e0ac74d9f424b79d9119444f0c164455f43
Mart-Matteus Kampus <mkampus@gmail.com>
2021-04-27 15:43:02 +0300

    vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,83 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Min</h1>
+    <input v-model.number="minA">
+    <input v-model.number="minB">
+    <button v-on:click="min()">Min</button>
+    {{ minAnswer }}
+    <h1>Max</h1>
+    <input v-model.number="maxA">
+    <input v-model.number="maxB">
+    <button v-on:click="max()">Max</button>
+    {{ maxAnswer }}
+    <h1>Abs</h1>
+    <input v-model.number="absA">
+    <button v-on:click="abs()">Return abs number</button>
+    {{ absAnswer }}
+    <h1>isEven?</h1>
+    <input v-model.number="isEvenA">
+    <button v-on:click="isEven()">Return if even.</button>
+    {{ isEvenAnswer }}
   </div>
 </template>
+<script>
+
+function isEven() {
+  if(this.isEvenA % 2 === 0){
+    this.isEvenAnswer = 'Is even!'
+  } else{
+    this.isEvenAnswer = 'Is NOT even!'
+  }
+
+}
+
+function abs() {
+  if(this.absA < 0){
+    this.absAnswer = this.absA * (-1);
+  } else{
+    this.absAnswer = this.absA;
+  }
+
+}
+
+function min() {
+  if (this.minA < this.minB) {
+    this.minAnswer = this.minA;
+  } else {
+    this.minAnswer = this.minB;
+  }
+}
+
+function max(){
+  if (this.maxA<this.maxB){
+    this.maxAnswer = this.maxB
+  } else {
+    this.maxAnswer = this.maxA
+  }
+
+}
+
+
+export default {
+  data: function () {
+    return {
+      'minA': 0,
+      'minB': 0,
+      'minAnswer': 0,
+      'maxA': 0,
+      'maxB': 0,
+      'maxAnswer': 0,
+      'absA':0,
+      'absAnswer': 0,
+      'isEvenA' : 0,
+      'isEvenAnswer': 0,
+    }
+  },
+  methods: {
+    'min': min,
+    'max': max,
+    'abs': abs,
+    'isEven': isEven,
+  }
+}
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div>
+      <input placeholder="Enter your email" v-model="email"/>
+      <button v-on:click="register">Register</button>
+      <table>
+        <tr v-for="email in emails">
+          <td>{{email}}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      'email': '',
+      'emails': ["a@a.ee", "test@test.ee"]
+    }
+  },
+  methods: {
+    'register': function () {
+      this.emails.push(this.email);
+    }
+
+  }
+}
+</script>
